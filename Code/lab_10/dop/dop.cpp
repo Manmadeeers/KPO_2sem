5df66d87d12c17d3de3b2d7bbb746de06c9053f7
Manmad <philmade6@gmail.com>
2024-05-17 09:58:52 +0300

    lab10

---
+++ Code/lab_10/dop/dop.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+	auto check_number = [](auto number) {
+		return number > 0;
+	};
+	auto number1 = check_number(-100);
+	cout << number1 << endl;
+	auto number2 = check_number(100);
+	cout << number2 << endl;
+	auto number3 = check_number(0);
+	cout << number3 << endl;
+	auto number4 = check_number(25.164);
+	cout << number4 << endl;
+	auto number5 = check_number('1');
+	cout << number5 << endl;
+}
